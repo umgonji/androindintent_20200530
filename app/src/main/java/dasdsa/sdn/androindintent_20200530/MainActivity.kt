51035a3443b5b86c3306515f0a689a9568c2789d
umgonji <umgonji@naver.com>
2020-05-30 10:46:04 +0900

    실제 전화 연결 CALL 액션 테스트  => 권한 획득 못해서 앱이 강제종료됨

--- app/src/main/java/dasdsa/sdn/androindintent_20200530/MainActivity.kt
+++ app/src/main/java/dasdsa/sdn/androindintent_20200530/MainActivity.kt
@@ -22,5 +22,19 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+        callBtn.setOnClickListener {
+            //타이핑 해둔 폰번을 받아오자 (String 으로)
+            val phoneNum = phoneNumEdt.text.toString()
+
+            //어디로 전화를 걸지 정보를 저장하는 Uri를 만들자
+            val myUri = Uri.parse("tel:${phoneNum}")
+
+            //전화를 걸어줄 안드로이드 전화앱 화면 호출
+            //어떤 종류의 행동 + 어디로 보낼지를 Intent에 넣어주자.
+            val myIntent = Intent(Intent.ACTION_CALL, myUri)
+            startActivity(myIntent)
+
+        }
+
     }
 }
